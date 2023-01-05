392bc9e69a31bef704913bac018ea07af6638aa7
bchjeon01 <root@DESKTOP-PIJEJ06.localdomain>
2023-01-06 00:43:04 +0900

    First

--- 3-3.c
+++ 3-3.c
@@ -1,54 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int search_idx(const int a[], int n, int key, int idx[]);
+
+int search_idx(const int a[], int n, int key, int idx[])
 {
 	int i=0;
 	int m=0;
 	int j=0;
-	for(i=0;i<n;i++)
-	{
+	for(i=0;i<n;i++) {
 		if (a[i]==key)
 			m++;
-		i++;
-		if (m==0)
-			return -1;
 	}
+	if (m==0)
+		return -1;
 	idx = calloc(m, sizeof(int));
-	for(i=1;i<n;i++)
-	{
-		if (a[i]==key)
+	
+	for(i=1;i<n;i++) {
+		if (a[i]==key) {
 			idx[j]=a[i-1];
 			j++;
-		i++;
+		}
 	}
+	printf("%d....", idx[0]);
+	
+	for(i=0;i<m;i++) {
+		printf("%d/", idx[i]);
+	}
+	
 	return m;
 }
 
 int main(void)
 {
-	int i, nx, ky, idx;
+	int i, nx, ky, idx, num;
 	int *p;
-	int *index;
+	int *index=NULL;
 	printf("요소 개수 : ");
 	scanf("%d", &nx);
-	p = calloc(nx, sizeof(int));
+	p = (int *)calloc(nx, sizeof(int));
 	for(i=0;i<nx;i++)
 	{
-		printf("x[%d] : ", i);
-		scanf("%d", &x[i]);
+		printf("p[%d] : ", i);
+		scanf("%d", &p[i]);
 	}
+	
 	printf("키 : ");
 	scanf("%d", &ky);
-	search_idx(p, nx, index);
-	if (search_idx == -1)
+	
+	num = search_idx(p, nx, ky, index);
+	if (num == -1)
 		printf("검색실패");
 	else
-		printf("%d", m);
-	for(i=0;i<m;i++)
-	{
-		printf("%d", index[i]);
-	}
+		printf("%d", num);
+
 	free(p);
 	free(index);
 }
